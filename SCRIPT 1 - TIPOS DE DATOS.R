e5506f81c663c8eed87e2e44d26c3f631ba1cdae
Dylan TR <69126491+Dylan0621@users.noreply.github.com>
2020-12-04 10:58:27 -0500

    Update SCRIPT 1 - TIPOS DE DATOS.R

--- SCRIPT 1 - TIPOS DE DATOS.R
+++ SCRIPT 1 - TIPOS DE DATOS.R
@@ -13,13 +13,14 @@ orangeec = read.csv("orangeec.csv")
 
 # Conocer los tipos de datos ___________________________________________________
 
-str(mtcars) # Permite concoer la estructura del data set
+str(mtcars) # Permite conocer la estructura del data set
 
 ?mtcars
 
 class(mtcars$vs) # Para ver la estructura de una variable
                  # '$' Permite acceder a una variable
 class(mtcars$am)
+
 
 mtcars$vs = as.logical(mtcars$vs) # Permite comvertir la clase de un valor
 mtcars$am = as.logical(mtcars$am) # EJ: Convertir de int a log
@@ -28,9 +29,31 @@ mtcars$am = as.logical(mtcars$am) # EJ: Convertir de int a log
 # Explorar los datos de orangeec
 
 str(orangeec) # 17 obs. of  13 variables
-dim(orangeec) # Permite conocer claramente el numero de observaciones y variables
+dim(orangeec) # Permite conocer claramente el número de observaciones y variables
+ncol(orangeec)
+nrow(orangeec)
+names(orangeec)
+summary(orangeec)
+
+#_______________________________________________________________________________
+
+# TRANSFORMACIÓN DE DATO EN MTCARS _____________________________________________
+
+summary(mtcars)
+
+wt <- (mtcars$wt * 1000) / 2.20462262
+wt # Esta variable, otorga los valores en libras.Para lo cual, los transformaré 
+   # a Kg en el dataset
+
+# Algunos datos relevantes son
+# 1 kg = 2.20462262 lb
+# 1 lb = 0.45359237 kg
+# NIST, Instituto Nacional de Estándares y Tecnología de los Estados Unidos, 
+# recomienda la siguiente aproximación: 1 kg = 2.20 lb 
 
 
+mtcars.new <- transform(mtcars,wt = wt * 1000 / 2.20462262)
+summary(mtcars.new)
 
 
 
